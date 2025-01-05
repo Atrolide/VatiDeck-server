2cd5585643cfe0ac0a8f00cc96752ccbf07d3cd0
Atrolide <Atrolide@users.noreply.github.com>
2025-01-05 16:49:04 +0100

    feat(go): Added a basic server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,86 @@
+// Package main starts the VatiDeck server.
 package main
+
+import (
+	"context"
+	"github.com/Atrolide/VatiDeck-server/cmd/routes"
+	"github.com/Atrolide/VatiDeck-server/pkg/logger"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// TODO: Add comments for better documentation
+
+const (
+	// Port to listen on
+	port = ":8080"
+
+	// Timeouts
+	readTimeout     = 10 * time.Second
+	writeTimeout    = 10 * time.Second
+	shutdownTimeout = 5 * time.Second
+)
+
+func main() {
+	// Initialize logger
+	log := logger.InitLogger()
+
+	// Log server start
+	log.Info("Starting VatiDeck server on port " + port)
+
+	// Create a custom Gorilla mux (router)
+	router := mux.NewRouter()
+
+	// Set up HTTP routes with the custom router
+	routes.SetupRoutes(router, log)
+
+	// Define an http.Server with custom settings
+	server := &http.Server{
+		Addr:         port,         // Port to listen on
+		Handler:      router,       // Attach Gorilla mux (router)
+		ReadTimeout:  readTimeout,  // Max time to read the request in seconds
+		WriteTimeout: writeTimeout, // Max time to write the response in seconds
+	}
+
+	// Channel for graceful shutdown
+	sigChan := make(chan os.Signal, 1) // (•ᴗ•)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start server
+	go func() {
+		log.Info("VatiDeck server started on port " + port)
+		if err := server.ListenAndServe(); err != nil {
+			if err == http.ErrServerClosed {
+				log.Info("Server closed")
+			} else {
+				log.Error("Error starting server on port " + port + ": " + err.Error())
+			}
+		}
+	}()
+
+	// Wait for an interrupt signal
+	<-sigChan // (•ᴗ•)
+	// Create context with a 5-second timeout for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	shutdownServer(ctx, server, log)
+}
+
+// REVIEW: Move to a separate package?
+
+// shutdownServer handles graceful shutdown of the server.
+func shutdownServer(ctx context.Context, server *http.Server, log *logger.Logger) {
+	log.Info("Shutting down server...")
+
+	// Graceful shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("Error during shutdown: " + err.Error())
+	} else {
+		log.Info("Server gracefully stopped")
+	}
+}
